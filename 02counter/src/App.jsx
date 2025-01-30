e8f64433867823ff23f8ce467bca23d689a1b110
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-01-30 00:31:17 +0000

    Counter Projet

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+function App() {
+
+  const [counter , setCounter] = useState(15);
+
+
+  const addValue = () => {
+    setCounter((preAdd) => preAdd === 20 ? 20 : preAdd = preAdd + 1)
+  }
+
+  const removeValue = () => {
+    setCounter((preRemove) => preRemove === 0 ? 0 : preRemove = preRemove - 1)
+  }
+
+  return (
+    <>
+      <h1>chai ar react</h1>
+      <h2>Counter number is {counter}</h2>
+
+      <button onClick={addValue} >Add number</button>
+      <br />
+      <button onClick={removeValue}>Remove number</button>
+    </>
+  )
+}
+
+export default App
